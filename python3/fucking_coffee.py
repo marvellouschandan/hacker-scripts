33039ef0f5fa5423d4c8658567cafa9a5b2c0ac5
mukesh9696 <9696mks@gmail.com>
2019-10-02 12:11:11 +0530

    Add Some comments at proper place and remove indentation error

--- python3/fucking_coffee.py
+++ python3/fucking_coffee.py
@@ -13,22 +13,22 @@ COFFEE_MACHINE_PROM = 'Password: '
 
 def main():
     # Exit early if no sessions with my_username are found.
-    if not any(s.startswith(b'my_username ') for s in sh('who').split(b'\n')):
+    if not any(s.startswith(b'my_username') for s in sh('who').split(b'\n')):
         return
 
-    time.sleep(17)
+    time.sleep(17) # sleep system for 17 seconds
 
-    conn = telnetlib.Telnet(host=COFFEE_MACHINE_ADDR)
+    conn = telnetlib.Telnet(host=COFFEE_MACHINE_ADDR)  #creating the connection
     conn.open()
     conn.expect([COFFEE_MACHINE_PROM])
     conn.write(COFFEE_MACHINE_PASS)
 
     conn.write('sys brew')
-    time.sleep(64)
+    time.sleep(64)    # sleep system for 16 seconds
 
     conn.write('sys pour')
-    conn.close()
+    conn.close()     # closing the connection
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  #check__name__ is equal to __main__ or  not.
     main()
